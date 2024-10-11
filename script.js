2e265647feb8ed5300cfc3ac60b875afa2d95bd0
Mahika <mahikasharma0007@gmail.com>
2024-10-11 21:58:15 +0530

    uploaded

---
+++ script.js
@@ -0,0 +1,176 @@
+
+
+(function () {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /**
+     * Navbar links active state on scroll
+     */
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+    /**
+     * Scrolls to an element with header offset
+     */
+    const scrollto = (el) => {
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos,
+            behavior: 'smooth'
+        })
+    }
+
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function (e) {
+        select('body').classList.toggle('mobile-nav-active')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
+
+    /**
+     * Scrool with ofset on links with a class name .scrollto
+     */
+    on('click', '.scrollto', function (e) {
+        if (select(this.hash)) {
+            e.preventDefault()
+
+            let body = select('body')
+            if (body.classList.contains('mobile-nav-active')) {
+                body.classList.remove('mobile-nav-active')
+                let navbarToggle = select('.mobile-nav-toggle')
+                navbarToggle.classList.toggle('bi-list')
+                navbarToggle.classList.toggle('bi-x')
+            }
+            scrollto(this.hash)
+        }
+    }, true)
+
+    /**
+     * Scroll with ofset on page load with hash links in the url
+     */
+    window.addEventListener('load', () => {
+        if (window.location.hash) {
+            if (select(window.location.hash)) {
+                scrollto(window.location.hash)
+            }
+        }
+    });
+    /**
+       * Animation on scroll
+       */
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
+    /**
+  * Hero type effect
+  */
+    const typed = select('.typed')
+    if (typed) {
+        let typed_strings = typed.getAttribute('data-typed-items')
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+
+})()
+
+
+function exp_content(element) {
+    const tabIndex = element.getAttribute('tabindex');
+    const paraToShow = document.querySelector(`.exp_para[tabindex="${tabIndex}"]`);
+    const boxToShow = document.querySelector(`.exp_content[tabindex="${tabIndex}"]`);
+
+    if (paraToShow.style.display === 'block') {
+        paraToShow.style.display = 'none';
+        boxToShow.style.backgroundColor = "#5A72A0";
+    } else {
+        // Hide all paragraphs
+        document.querySelectorAll('.exp_para').forEach(para => {
+            para.style.display = 'none';
+
+        });
+        document.querySelectorAll('.exp_content').forEach(box => {
+            box.style.backgroundColor = "#5A72A0";
+
+        });
+        // Show the clicked paragraph
+        paraToShow.style.display = 'block';
+        boxToShow.style.backgroundColor = "#314261";
+    }
+}
+
